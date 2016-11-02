d4979b9f1b26cbfd140a5e0d083f60dfc3b8efe9
Yedarm Seong <mybirth0407@gmail.com>
2016-11-03 00:31:20 +0900

    커밋

---
+++ SplashExample/app/src/main/java/bit/splashexample/MainActivity.java
@@ -0,0 +1,17 @@
+package bit.splashexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
+
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+    }
+}
